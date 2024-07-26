6ce1aa8315f9b314c49415c5a22ef4298c64c5f4
muzammal831 <122962287+muzammal831@users.noreply.github.com>
2024-07-26 11:09:31 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
                 <Route path="/admin/rewards" element={<RewardsList />} />
                         <Route path="/admin/rewards/create" element={<RewardCreate />} />
                         <Route path="/admin/rewards/:id/edit" element={<RewardEdit />} />
-                <Route path="/" element={<h1>Welcome to Laravel React App</h1>} />
+                <Route path="/" element={<h1>Welcome to Laravel React Application</h1>} />
                 <Route path="/admin/products" element={<ProductsList />} /> {/* Add ProductsList route */}
                 <Route path="/admin/products/create" element={<NewProduct />} /> {/* Route for creating a new product */}
                 <Route path="/admin/products/:id/edit" element={<EditProduct />} /> {/* Route for editing an existing product */}
